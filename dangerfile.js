7412819a73b859bf02d7203b20d9fc8824c50782
Piotr Wojtkowiak <pwojtkowiak@gmail.com>
2018-06-16 23:28:04 +0200

    WIP

--- dangerfile.js
+++ dangerfile.js
@@ -1,9 +1,17 @@
 import { message, warn, fail, danger } from 'danger';
 
-const modifiedMD = danger.git.modified_files.join('- ');
-message('Changed Files in this PR: \n - ' + modifiedMD);
+const JIRA_ISSUE_REGEXP = /^\[(\w+\-\d+)\]/;
+const BRANCH_FORMAT_REGEXP = /^(\w+?)\//;
 
+const modifiedMD = danger.git.modified_files.join('- ');
+const [, jiraId] = danger.github.pr.title.match(JIRA_ISSUE_REGEXP) || [];
+const [, changeType] =
+  danger.github.pr.head.ref.match(BRANCH_FORMAT_REGEXP) || [];
 const { pr } = danger.github;
+
+console.log(modifiedMD);
+
+message('Changed Files in this PR: \n - ' + modifiedMD);
 
 if (
   pr.assignee === null &&
@@ -14,13 +22,34 @@ if (
   warn('Please assign people who should review.');
 }
 
-if (!/^\[\w+\-\d+\]/.test(danger.github.pr.title)) {
+if (!jiraId) {
   warn(`PR title should start with task id, e.g: "[PROJECT-01] Issue title"`);
+} else {
   message(
     danger.utils.href(
-      `https://10clouds.atlassian.net/browse/CCE-190`,
-      'Jira issue'
+      `https://10clouds.atlassian.net/browse/${jiraId}`,
+      'Go to issue page'
     )
   );
-} else {
+}
+
+if (
+  !changeType ||
+  !['wip', 'feature', 'bugfix', 'junk'].includes(changeType.toLowerCase())
+) {
+  warn(`
+Please use well-defined tokens in your branch name:
+
+wip - Works in progress; stuff I know won't be finished soon
+feature - Feature I'm adding or expanding
+bugfix - Bug fix or experiment
+junk - Throwaway branch created to experiment
+
+E.g: feature/libraries-upgrade
+
+This allow us to quick search branches:
+$ git branch --list "feature/*"
+feature/foo
+feature/frabnotz
+  `);
 }

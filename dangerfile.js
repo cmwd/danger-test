d0c3b1ff697032c3a7291972e049f38c2f4a3f80
Piotr Wojtkowiak <pwojtkowiak@gmail.com>
2018-06-16 22:28:39 +0200

    WIP

--- dangerfile.js
+++ dangerfile.js
@@ -1,10 +1,15 @@
-import { message, warn, danger } from 'danger';
+import { message, warn, fail, danger } from 'danger';
 
 const modifiedMD = danger.git.modified_files.join('- ');
-const reviewers = danger.github.requested_reviewers;
-
 message('Changed Files in this PR: \n - ' + modifiedMD);
 
-if (reviewers.length === 0) {
-  warn('Please assign reviewers');
+if (pr.assignee === null) {
+  const method = pr.title.includes('WIP') ? warn : fail;
+  method(
+    'Please assign someone to merge this PR, and optionally include people who should review.'
+  );
+}
+
+if (!/^\[\w+\-\d+\]/.test(danger.github.pr.title)) {
+  warn(`PR title should start with task id, e.g: "[PROJECT-01] Issue title"`);
 }

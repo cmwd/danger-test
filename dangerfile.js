11765f9464735316a97da08f3ec44fac80ac00b3
Piotr Wojtkowiak <pwojtkowiak@gmail.com>
2018-06-16 22:50:47 +0200

    WIP

--- dangerfile.js
+++ dangerfile.js
@@ -3,13 +3,24 @@ import { message, warn, fail, danger } from 'danger';
 const modifiedMD = danger.git.modified_files.join('- ');
 message('Changed Files in this PR: \n - ' + modifiedMD);
 
-if (danger.github.pr.assignee === null) {
-  const method = pr.title.includes('WIP') ? warn : fail;
-  method(
-    'Please assign someone to merge this PR, and optionally include people who should review.'
-  );
+const { pr } = danger.github;
+
+if (
+  pr.assignee === null &&
+  !pr.labels.some(({ name }) =>
+    ['wip', 'work in progress'].includes(name.toLowerCase())
+  )
+) {
+  warn('Please assign people who should review.');
 }
 
 if (!/^\[\w+\-\d+\]/.test(danger.github.pr.title)) {
   warn(`PR title should start with task id, e.g: "[PROJECT-01] Issue title"`);
+  message(
+    danger.utils.href(
+      `https://10clouds.atlassian.net/browse/CCE-190`,
+      'Jira issue'
+    )
+  );
+} else {
 }

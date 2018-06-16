f299c2114b6c56fd4c7dcc6d47d491b7aa426d98
Piotr Wojtkowiak <pwojtkowiak@gmail.com>
2018-06-16 22:15:51 +0200

    WIP

--- dangerfile.js
+++ dangerfile.js
@@ -1,4 +1,10 @@
-import { message, danger } from 'danger';
+import { message, warn, danger } from 'danger';
 
 const modifiedMD = danger.git.modified_files.join('- ');
+const reviewers = danger.github.requested_reviewers;
+
 message('Changed Files in this PR: \n - ' + modifiedMD);
+
+if (reviewers.length === 0) {
+  warn('Please assign reviewers');
+}
